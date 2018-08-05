7d3aada1a7aa4e84f7ada288d86215e17a88b9e0
mjay2018 <41263691+mjay2018@users.noreply.github.com>
2018-08-05 22:45:39 +0530

    Add files via upload

--- vgg.py
+++ vgg.py
@@ -4,23 +4,24 @@ Created on Sun Jul 29 13:27:39 2018
 
 @author: Mritunjay
 """
-
-from keras.layers import Input, Lambda, Dense, Flatten
+import keras
+from keras.layers import Input, Lambda, Dense, Flatten,GlobalAveragePooling2D,Conv2D,MaxPooling2D, AvgPool2D
 from keras.models import Model
 from keras.applications.vgg16 import VGG16
+from keras.optimizers import SGD, adam,adamax
 from keras.applications.vgg16 import preprocess_input
-from keras.layers import Dropout
+from keras.layers import Dropout, BatchNormalization
 from keras.utils import np_utils
 from keras.preprocessing import image
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
-from keras.datasets import cifar10
+
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
 import os
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
 
 
 
@@ -28,12 +29,12 @@ os.chdir('C:\\imagemodel')
 import Dataset_reader
 path = 'C:\\jbm\\All_61326\\All_61326\\train_61326'
 
-train_sample, train_label, val_sample, val_label = Dataset_reader.load_image(path=path, image_Size=128, val_size=.2)
+train_sample, train_label, val_sample, val_label = Dataset_reader.load_image(path=path, image_Size=128, val_size=.5)
 train_label = np_utils.to_categorical(train_label,len(os.listdir(path)))
 val_label = np_utils.to_categorical(val_label, len(os.listdir(path)))
 IMAGE_SIZE = [128, 128] # feel free to change depending on dataset
-epochs = 25
-batch_size = 8
+epochs = 5000
+batch_size = 32
 
 vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
 
@@ -41,20 +42,34 @@ vgg = VGG16(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
 for layer in vgg.layers:
     layer.trainable = False
 
-
-
-# our layers - you can add more if you want
-
+#x = BatchNormalization()(vgg.output)
+#x = Conv2D(1024 ,(3,3),padding='same')(x)
+#x = BatchNormalization()(x)
+#x = MaxPooling2D(pool_size=(2 ,2),strides=(2,2))(x)
+#x = Conv2D(512,(1,2),padding='same')(x)
+#x = BatchNormalization()(x)
+#x = MaxPooling2D(pool_size=(1 ,2),strides=(1,2))(x)
+#x = BatchNormalization()(x)
 x = Flatten()(vgg.output)
-
-x = Dense(1000, activation='relu')(x)
-x = Dropout(.5)(x)
-x = Dense(200,activation=  'relu')(x)
+x = BatchNormalization()(x)
+x = Dropout(.2)(x)
+x = Dense(2048,activation='relu')(x)
+x = BatchNormalization()(x)
+x = Dropout(.2)(x)
+x = Dense(2048, kernel_initializer="random_uniform",activation='relu')(x)
+x = BatchNormalization()(x)
+x = Dropout(.2)(x)
+x = Dense(1024, activation='relu')(x)
+x = BatchNormalization()(x)
+x = Dropout(.2)(x)
+x = Dense(1024, activation='relu')(x)
+x = BatchNormalization()(x)
 x = Dropout(.2)(x)
 
-folders = glob.glob(path + '/*')
 
-prediction = (Dense(len(folders), activation='softmax')(x)
+folders = os.listdir(path)
+
+prediction = Dense(len(folders), activation='softmax')(x)
 
 
 # create a model object
@@ -63,18 +78,28 @@ model = Model(inputs=vgg.input, outputs=prediction)
 # view the structure of the model
 model.summary()
 # tell the model what cost and optimization method to use
+optim = SGD(lr=0.00001,momentum=.9,nesterov=True)
 
 model.compile(
         loss='categorical_crossentropy',
-        optimizer='rmsprop',
+        optimizer='adam',
         metrics=['accuracy']
         )
 
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+best_weights_filepath = 'c://jbm///best_weights.hdf5'
+earlyStopping=EarlyStopping(monitor='val_loss', patience=100, verbose=1, mode='auto')
+saveBestModel = ModelCheckpoint(best_weights_filepath, monitor='val_loss',  save_best_only=True, mode='auto')
+callbacks_list = [saveBestModel]
+
 data_gen = ImageDataGenerator(
-        rotation_range=20,
+        featurewise_center=False,
+        featurewise_std_normalization=False,
+        rotation_range=40,
         width_shift_range=0.1,
         height_shift_range=0.1,
-        shear_range=0.1,
+        shear_range=0.1, 
         zoom_range=0.2,
         horizontal_flip=True,
         vertical_flip=True,
@@ -85,8 +110,8 @@ train_gen = data_gen.flow(train_sample,train_label, batch_size=batch_size)
 test_gen= data_gen.flow(val_sample,val_label, batch_size=batch_size)
 
 
-model.fit_generator(data_gen.flow(train_sample, train_label, batch_size=batch_size),
-                    steps_per_epoch=len(train_sample) // batch_size, epochs=epochs)
+#model.fit_generator(data_gen.flow(train_sample, train_label, batch_size=batch_size),
+#                    steps_per_epoch=len(train_sample) // batch_size, epochs=epochs)
 
 fitmodel = model.fit_generator(
         train_gen,
@@ -94,4 +119,20 @@ fitmodel = model.fit_generator(
         epochs=epochs,
         steps_per_epoch=len(train_sample) // batch_size,
         validation_steps=len(val_sample) // batch_size,
+        callbacks=callbacks_list,
+        
         )
+
+
+fitmodel = model.fit(
+        x = train_sample,
+        y = train_label,
+        validation_data=(val_sample,val_label), 
+        batch_size = batch_size,
+        epochs=epochs,
+        #steps_per_epoch=len(train_sample) // batch_size,
+        #validation_steps=len(val_sample) // batch_size,
+        )
+
+
+model.save('vgg_model.h5')
